bfb625c4d2562b80cc482874ea94c69ba505b44f
Wojciech Nowak <kontakt@wnowak.pl>
2020-11-25 05:49:50 +0100

    Created DTO for webapi

---
+++ app/code/Monogo/Weather/Api/Data/WeatherInterface.php
@@ -0,0 +1,261 @@
+<?php
+declare(strict_types=1);
+
+namespace Monogo\Weather\Api\Data;
+
+/**
+ * Interface WeatherInterface
+ * @package Monogo\Weather\Api\Data
+ */
+interface WeatherInterface
+{
+    /**
+     * Description
+     */
+    public const DESCRIPTION = 'description';
+
+    /**
+     * City
+     */
+    public const CITY = 'city';
+
+    /**
+     * Country
+     */
+    public const COUNTRY = 'country';
+
+    /**
+     * Temperature
+     */
+    public const TEMPERATURE = 'temp';
+
+    /**
+     * Minumum temperature
+     */
+    public const MINIMUM_TEMPERATURE = 'temp_min';
+
+    /**
+     * Maximum temperature
+     */
+    public const MAXIMUM_TEMPERATURE = 'temp_max';
+
+    /**
+     * Wind icon id
+     */
+    public const WIND_ICON_ID = 'wind_icon_id';
+
+    /**
+     * Wind speed
+     */
+    public const WIND_SPEED = 'wind_speed';
+
+    /**
+     * Wind direction
+     */
+    public const WIND_DEG = 'wind_deg';
+
+    /**
+     * Pressure
+     */
+    public const PRESSURE = 'pressure';
+
+    /**
+     * Unit type
+     */
+    public const UNIT_TYPE = 'unit_type';
+
+    /**
+     * Metric
+     */
+    public const METRIC = 'metric';
+
+    /**
+     * Humidity
+     */
+    public const HUMIDITY = 'humidity';
+
+    /**
+     * Created at
+     */
+    public const CREATED_AT = 'created_at';
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription(string $description): self;
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription(): string;
+
+    /**
+     * Get city
+     *
+     * @param string $city
+     * @return $this
+     */
+    public function setCity(string $city): self;
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity(): string;
+
+    /**
+     * Set country code
+     *
+     * @param string $country
+     * @return $this
+     */
+    public function setCountry(string $country): self;
+
+    /**
+     * Get country code
+     *
+     * @return string
+     */
+    public function getCountry(): string;
+
+    /**
+     * Set temperature
+     *
+     * @param float $temp
+     * @return $this
+     */
+    public function setTemp(float $temp): self;
+
+    /**
+     * Get temperature
+     *
+     * @return string
+     */
+    public function getTemp(): string;
+
+    /**
+     * Set maximum temperature
+     *
+     * @param float $temp
+     * @return $this
+     */
+    public function setMaxTemp(float $temp): self;
+
+    /**
+     * Get maximum temperature
+     *
+     * @return float
+     */
+    public function getMaxTemp(): float;
+
+    /**
+     * Set minimum temperature
+     *
+     * @param float $temp
+     * @return $this
+     */
+    public function setMinTemp(float $temp): self;
+
+    /**
+     * Get minimum temperature
+     *
+     * @return float
+     */
+    public function getMinTemp(): float;
+
+    /**
+     * Set wind icon id
+     *
+     * @param string $iconId
+     * @return $this
+     */
+    public function setWindIconId(string $iconId): self;
+
+    /**
+     * Get wind icon id
+     *
+     * @return string
+     */
+    public function getWindIconId(): string;
+
+    /**
+     * Set wind speed
+     *
+     * @param int $speed
+     * @return $this
+     */
+    public function setWindSpeed(int $speed): self;
+
+    /**
+     * Get wind speed
+     *
+     * @return int
+     */
+    public function getWindSpeed(): int;
+
+    /**
+     * Set wind direction
+     *
+     * @param int $deg
+     * @return $this
+     */
+    public function setWindDeg(int $deg): self;
+
+    /**
+     * Get wind direction
+     *
+     * @return int
+     */
+    public function getWindDeg(): int;
+
+    /**
+     * Set pressure
+     *
+     * @param int $pressure
+     * @return $this
+     */
+    public function setPressure(int $pressure): self;
+
+    /**
+     * Get pressure
+     *
+     * @return int
+     */
+    public function getPressure(): int;
+
+    /**
+     * Set unit
+     *
+     * @param string $type
+     * @return $this
+     */
+    public function setUnit(string $type): self;
+
+    /**
+     * Get unit
+     *
+     * @return string
+     */
+    public function getUnit(): string;
+
+    /**
+     * Set created at
+     *
+     * @param $date
+     * @return $this
+     */
+    public function setCreatedAt($date): self;
+
+    /**
+     * Get created at
+     *
+     * @return string
+     */
+    public function getCreatedAt(): string;
+}
--- app/code/Monogo/Weather/Model/Weather.php
+++ app/code/Monogo/Weather/Model/Weather.php
@@ -5,12 +5,13 @@ namespace Monogo\Weather\Model;
 
 use Magento\Framework\Model\AbstractModel;
 use Monogo\Weather\Model\ResourceModel\Weather as WeatherResource;
+use Monogo\Weather\Api\Data\WeatherInterface;
 
 /**
  * Class Weather
  * @package Monogo\Weather\Model
  */
-class Weather extends AbstractModel
+class Weather extends AbstractModel implements WeatherInterface
 {
     /**
      * {@inheritDoc}
@@ -18,5 +19,197 @@ class Weather extends AbstractModel
     protected function _construct()
     {
         $this->_init(WeatherResource::class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDescription(string $description): self
+    {
+        return $this->setData(WeatherInterface::DESCRIPTION, $description);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescription(): string
+    {
+        return $this->getData(WeatherInterface::DESCRIPTION);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setCity(string $city): self
+    {
+        return $this->setData(WeatherInterface::CITY, $city);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCity(): string
+    {
+        return $this->getData(WeatherInterface::CITY);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setCountry(string $country): self
+    {
+        return $this->setData(WeatherInterface::COUNTRY, $country);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCountry(): string
+    {
+        return $this->getData(WeatherInterface::COUNTRY);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setTemp(float $temp): self
+    {
+        return $this->setData(WeatherInterface::TEMPERATURE, $temp);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTemp(): string
+    {
+        return $this->getData(WeatherInterface::TEMPERATURE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setMaxTemp(float $temp): self
+    {
+        return $this->setData(WeatherInterface::MAXIMUM_TEMPERATURE, $temp);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMaxTemp(): float
+    {
+        return $this->getData(WeatherInterface::MAXIMUM_TEMPERATURE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setMinTemp(float $temp): self
+    {
+        return $this->setData(WeatherInterface::MINIMUM_TEMPERATURE, $temp);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMinTemp(): float
+    {
+        return $this->getData(WeatherInterface::MINIMUM_TEMPERATURE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setWindIconId(string $iconId): self
+    {
+        return $this->setData(WeatherInterface::WIND_ICON_ID, $iconId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getWindIconId(): string
+    {
+        return $this->getData(WeatherInterface::WIND_ICON_ID);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setWindSpeed(int $speed): self
+    {
+        return $this->setData(WeatherInterface::WIND_SPEED, $speed);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getWindSpeed(): int
+    {
+        return $this->getData(WeatherInterface::WIND_SPEED);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setWindDeg(int $deg): self
+    {
+        return $this->setData(WeatherInterface::WIND_DEG, $deg);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getWindDeg(): int
+    {
+        return $this->getData(WeatherInterface::WIND_DEG);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setPressure(int $pressure): self
+    {
+        return $this->setData(WeatherInterface::PRESSURE, $pressure);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPressure(): int
+    {
+        return $this->getData(WeatherInterface::PRESSURE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUnit(string $type): self
+    {
+        return $this->setData(WeatherInterface::UNIT_TYPE, $type);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUnit(): string
+    {
+        return $this->getData(WeatherInterface::UNIT_TYPE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setCreatedAt($date): self
+    {
+        return $this->setData(WeatherInterface::CREATED_AT, $date);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->getData(WeatherInterface::CREATED_AT);
     }
 }
